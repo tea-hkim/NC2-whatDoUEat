2d8918b363e6572e44432898d1fe48a8831a54cf
Nick(taehoKim) <xogh8025@gmail.com>
2022-08-30 10:11:24 +0900

    add : 프로젝트 생성

---
+++ WhatDoUEat/WhatDoUEat/WhatDoUEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatDoUEatApp.swift
+//  WhatDoUEat
+//
+//  Created by 김태호 on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct WhatDoUEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
