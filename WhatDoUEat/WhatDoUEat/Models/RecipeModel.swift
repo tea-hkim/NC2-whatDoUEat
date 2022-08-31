89016e61ebc6352d03901b8e58dffd2c6f601304
Nick(taehoKim) <xogh8025@gmail.com>
2022-08-31 16:07:16 +0900

    feat: 유저 사진 받아오는 기능 추가

--- WhatDoUEat/WhatDoUEat/Models/RecipeModel.swift
+++ WhatDoUEat/WhatDoUEat/Models/RecipeModel.swift
@@ -5,4 +5,25 @@
 //  Created by 김태호 on 2022/08/31.
 //
 
-import Foundation
+import SwiftUI
+
+enum Ingredients: Int {
+    case 감자 = 0
+    case 당근 = 1
+    case 무
+    case 브로콜리
+    case 양배추
+    case 오이
+    case 완두콩
+    case 콜리플라워
+    case 토마토
+    case 파프리카
+    case 호박
+}
+
+struct RecipeModel {
+    var recipeName: String
+    var ingredients: [Ingredients]
+    var cookingProcess: [String]
+}
+
--- WhatDoUEat/WhatDoUEat/View/ImagePicker.swift
+++ WhatDoUEat/WhatDoUEat/View/ImagePicker.swift
@@ -1,5 +1,5 @@
 //
-//  ImagePickerView.swift
+//  ImagePicker.swift
 //  WhatDoUEat
 //
 //  Created by 김태호 on 2022/08/31.
@@ -38,10 +38,29 @@ struct ImagePicker : UIViewControllerRepresentable {
         }
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            guard let selectedImage = info[.originalImage] as? UIImage else { return }
+            guard let selectedImage = info[.originalImage] as? UIImage else {
+                fatalError("Picker didn't have an original image.")
+            }
+            
             let data = selectedImage.pngData()
             self.picker.selectedImage = data!
             self.picker.isPresented.toggle()
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.classifyImage(selectedImage)
+            }
+        }
+    }
+}
+
+extension ImagePicker {
+    
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
         }
     }
 }
